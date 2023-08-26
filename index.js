ee81c0bfae9fbf97682007b169179e7dcf5dc330
danlourenco <danlourenco@gmail.com>
2023-08-25 23:05:21 -0400

    1 file modified

--- index.js
+++ index.js
@@ -17,13 +17,6 @@ const mockPickerTimes = [
   },
 ];
 
-// export const toZoned = (arr) =>
-//   arr.map((obj) =>
-//     Object.values(obj).map((instant) => instant.toZonedDateTimeISO(timeZone))
-//   );
-
-// const zoned = mockPickerTimes.map(toZoned);
-// console.log(zoned);
 export const toIntervals = (pickerTimes) =>
   new Interval(
     Temporal.Instant.from(pickerTimes.minimum).toZonedDateTimeISO(timeZone),
@@ -31,51 +24,20 @@ export const toIntervals = (pickerTimes) =>
   );
 
 const arrayOfIntervals = mockPickerTimes.map(toIntervals);
-console.log(arrayOfIntervals);
-/*
-[
-  Interval {
-    start: Temporal.Instant <2023-08-25T23:45:00Z>,
-    end: Temporal.Instant <2023-08-26T00:15:00Z>
-  },
-  Interval {
-    start: Temporal.Instant <2023-08-26T23:45:00Z>,
-    end: Temporal.Instant <2023-08-27T00:15:00Z>
-  }
-]
-*/
 
+const generateSelectOption = (timeSlot) => ({
+  text: timeSlot.toLocaleString('en-US', {
+    hour: 'numeric',
+    minute: 'numeric',
+  }),
+  value: timeSlot.toInstant(),
+});
 const selectData = arrayOfIntervals.map((interval, index, timeZone) => ({
   key: index,
   slots: Array.from(
     interval.iterate(Temporal.Duration.from({ minutes: intervalIncrement })),
-    (timeSlot, i, timeZone) => ({
-      text: timeSlot.toLocaleString('en-US', {
-        hour: 'numeric',
-        minute: 'numeric',
-      }),
-      value: timeSlot.toInstant(),
-    })
+    generateSelectOption
   ),
 }));
-
-/*
-[
-  {
-    key: 0,
-    slots: [
-      Temporal.Instant <2023-08-25T23:45:00Z>,
-      Temporal.Instant <2023-08-26T00:00:00Z>
-    ]
-  },
-  {
-    key: 1,
-    slots: [
-      Temporal.Instant <2023-08-26T23:45:00Z>,
-      Temporal.Instant <2023-08-27T00:00:00Z>
-    ]
-  }
-]
-*/
 
 console.log(JSON.stringify(selectData));
